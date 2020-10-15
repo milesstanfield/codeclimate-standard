ac751bdbfe51c7ea78fa1eca7460c798c77055b2
Jake Frautschi <jake@ifttt.com>
2020-10-14 17:20:16 -0700

    Fix some specs

--- spec/cc/engine/file_list_resolver_spec.rb
+++ spec/cc/engine/file_list_resolver_spec.rb
@@ -1,5 +1,5 @@
 require "spec_helper"
-require "rubocop"
+require "standard"
 require "cc/engine/file_list_resolver"
 
 module CC::Engine
@@ -7,14 +7,14 @@ module CC::Engine
     include FilesystemHelpers
 
     before { @code = Dir.mktmpdir }
-    let(:rubocop_config) { RuboCop::ConfigStore.new }
+    let(:builds_config) { ::Standard::BuildsConfig.new.call([]) }
 
     it "uses default include path" do
       Dir.chdir(@code) do
         create_source_file("a.rb", "def a; true; end")
         create_source_file("not_ruby.txt", "some text")
 
-        resolver = FileListResolver.new(root: @code, engine_config: {}, config_store: rubocop_config)
+        resolver = FileListResolver.new(root: @code, engine_config: {}, builds_config: builds_config)
         expect(resolver.expanded_list).to eq [Pathname.new("a.rb").realpath.to_s]
       end
     end
@@ -22,11 +22,11 @@ module CC::Engine
     it "finds ruby scripts without extensions" do
       Dir.chdir(@code) do
         create_source_file("a.rb", "def a; true; end")
-        create_source_file("bin/some_script", "#!/usr/bin/env ruby")
-        create_source_file("bin/some_other_script", "#!/usr/bin/env sh")
+        create_source_file("scripts/some_script", "#!/usr/bin/env ruby")
+        create_source_file("scripts/some_other_script", "#!/usr/bin/env sh")
 
-        resolver = FileListResolver.new(root: @code, engine_config: {}, config_store: rubocop_config)
-        expect(resolver.expanded_list).to eq %w[a.rb bin/some_script].map { |fn| Pathname.new(fn).realpath.to_s }
+        resolver = FileListResolver.new(root: @code, engine_config: {}, builds_config: builds_config)
+        expect(resolver.expanded_list).to eq %w[a.rb scripts/some_script].map { |fn| Pathname.new(fn).realpath.to_s }
       end
     end
 
@@ -35,25 +35,35 @@ module CC::Engine
         create_source_file("a.rb", "def a; true; end")
         create_source_file("src/b.rb", "def a; true; end")
 
-        resolver = FileListResolver.new(root: @code, engine_config: {"include_paths" => %w[src/]}, config_store: rubocop_config)
+        resolver = FileListResolver.new(root: @code, engine_config: {"include_paths" => %w[src/]}, builds_config: builds_config)
         expect(resolver.expanded_list).to eq [Pathname.new("src/b.rb").realpath.to_s]
       end
     end
 
-    it "respects rubocop excludes" do
+    it "respects default ignore list" do
+      Dir.chdir(@code) do
+        create_source_file("vendor/foo/a.rb", "def a; true; end")
+        create_source_file("bin/b.rb", "def a; true; end")
+        create_source_file("tmp/c.rb", "def a; true; end")
+        create_source_file("db/schema.rb", "ActiveRecord::Schema.define(:version => #{Time.current.to_i}) do; end")
+
+        resolver = FileListResolver.new(root: @code, engine_config: {}, builds_config: builds_config)
+        expect(resolver.expanded_list).to be_empty
+      end
+    end
+
+    it "respects ignore list in standard config" do
       Dir.chdir(@code) do
         create_source_file("Gemfile", "source 'https://rubygems.org'")
         create_source_file("src/b.rb", "def a; true; end")
         create_source_file("src/c.rb", "def a; true; end")
-        create_source_file(".rubocop.yml", <<~YML)
-          AllCops:
-            NewCops: enable
-            Exclude:
-              - src/c.rb
-              - Gemfile
-        YML
+        create_source_file(".standard.yml", <<~YAML)
+          ignore:
+            - src/c.rb
+            - Gemfile
+        YAML
 
-        resolver = FileListResolver.new(root: @code, engine_config: {"include_paths" => %w[Gemfile src/]}, config_store: rubocop_config)
+        resolver = FileListResolver.new(root: @code, engine_config: {}, builds_config: builds_config)
         expect(resolver.expanded_list).to eq [Pathname.new("src/b.rb").realpath.to_s]
       end
     end
@@ -62,7 +72,7 @@ module CC::Engine
       Dir.chdir(@code) do
         create_source_file("src/b.rb", "def a; true; end")
 
-        resolver = FileListResolver.new(root: @code, engine_config: {"include_paths" => %w[src/ public/assets]}, config_store: rubocop_config)
+        resolver = FileListResolver.new(root: @code, engine_config: {"include_paths" => %w[src/ public/assets]}, builds_config: builds_config)
         expect(resolver.expanded_list).to eq [Pathname.new("src/b.rb").realpath.to_s]
       end
     end
