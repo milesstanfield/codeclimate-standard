90fbd1362432fedd9fdad0762323e61a088aa919
Miles Stanfield <milesstanfield@gmail.com>
2024-11-23 06:49:55 -0500

    fixed undefined issue running specs described in rubocop/rubocop#11169 issue. borrowed #registry code from rubocop cop_helper

--- spec/support/standard_runner.rb
+++ spec/support/standard_runner.rb
@@ -4,6 +4,9 @@ require "tmpdir"
 module StandardRunner
   def self.included(example_group)
     example_group.include FilesystemHelpers
+    example_group.before do
+      allow_any_instance_of(RuboCop::AST::ProcessedSource).to receive(:registry).and_return(registry)
+    end
     example_group.around do |example|
       Dir.mktmpdir do |code|
         @code = code
@@ -21,10 +24,22 @@ module StandardRunner
   end
 
   def run_engine(config = nil)
+    @config = config
     io = StringIO.new
-    standard = CC::Engine::Standard.new(@code, config, io)
+    standard = CC::Engine::Standard.new(@code, @config, io)
     standard.run
 
     @engine_output = io.string
+  end
+
+  def registry
+    # https://github.com/rubocop/rubocop/blob/master/lib/rubocop/rspec/cop_helper.rb
+    @registry ||= begin
+      keys = RuboCop::Config.new(@config || {}, "#{Dir.pwd}/.rubocop.yml").keys
+      cops = keys.map { |directive| RuboCop::Cop::Registry.global.find_cops_by_directive(directive) }.flatten
+      cops << cop_class if defined?(cop_class) && !cops.include?(cop_class)
+      cops.compact!
+      RuboCop::Cop::Registry.new(cops)
+    end
   end
 end
